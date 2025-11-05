e6463eb2e6cbbf588b9227c00c25e71158779880
MiernikA <adrianmiernik@gmail.com>
2025-11-05 16:08:26 +0100

    wip: adjust resizing textures

--- src/ui/3d/models/GlobeCanvas.tsx
+++ src/ui/3d/models/GlobeCanvas.tsx
@@ -23,7 +23,7 @@ type Props = {
 };
 
 const Globe = ({ isMobile, rotationDelta }: Props) => {
-    const model = useGLTF("./models/planet/scene_final.glb");
+    const model = useGLTF("./models/planet/scene_optimized_soft.glb");
     const modelRef = useRef<Group>(null);
 
     useFrame((_, delta) => {
